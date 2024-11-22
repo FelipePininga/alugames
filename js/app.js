49cf505a3e283420e5ef14bb1320d256f7217414
FelipePininga <felipepininga@gmail.com>
2024-11-22 16:06:59 -0300

    Confirmando devoluções

--- js/app.js
+++ js/app.js
@@ -6,7 +6,9 @@ function alterarStatus(gameId){
         botao.classList.add('dashboard__item__button--return');
         jogo.querySelector('.dashboard__item__img').classList.add('dashboard__item__img--rented'); 
     } else { 
-        botao.textContent = "Alugar"; 
-        botao.classList.remove('dashboard__item__button--return'); 
-        jogo.querySelector('.dashboard__item__img').classList.remove('dashboard__item__img--rented'); }
+        if (confirm("Tem certeza de que deseja devolver este jogo?")){
+            botao.textContent = "Alugar"; 
+            botao.classList.remove('dashboard__item__button--return'); 
+            jogo.querySelector('.dashboard__item__img').classList.remove('dashboard__item__img--rented'); }
+        }
 }
