35ec6583efcd2383609b1cf8532456a767dde0e2
FelipePininga <felipepininga@gmail.com>
2024-11-22 15:37:20 -0300

    Implementando função que ativa botões

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function alterarStatus(gameId){
+    const jogo=document.getElementById(`game-${gameId}`);
+    const botao = jogo.querySelector('a.dashboard__item__button');
+    if (botao.textContent === "Alugar") { 
+        botao.textContent = "Devolver"; 
+        botao.classList.add('dashboard__item__button--return');
+        jogo.querySelector('.dashboard__item__img').classList.add('dashboard__item__img--rented'); 
+    } else { 
+        botao.textContent = "Alugar"; 
+        botao.classList.remove('dashboard__item__button--return'); 
+        jogo.querySelector('.dashboard__item__img').classList.remove('dashboard__item__img--rented'); }
+}
